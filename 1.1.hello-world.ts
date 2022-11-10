ea94925414eb150043987a61339e89c01d2b8f8c
paiDaXing-web <43596552+paiDaXing-web@users.noreply.github.com>
2022-11-10 20:56:54 +0800

    1 file added, 1 deleted

--- 1.1.hello-world.ts
+++ 1.1.hello-world.ts
@@ -6,6 +6,6 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>第一题:Hello World</h1><p> 你需要使得14行不会抛出异常</p>`;
 
 // 期望是一个 string 类型
-type HelloWorld = string;
+type HelloWorld = any;
 //你需要使得如下这行不会抛出异常
 type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
