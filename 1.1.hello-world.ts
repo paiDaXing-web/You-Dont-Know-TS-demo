44bd869d8745de48e073b3b473d471f33a62f746
paiDaXing-web <43596552+paiDaXing-web@users.noreply.github.com>
2022-11-18 21:07:02 +0800

    1 file modified

--- 1.1.hello-world.ts
+++ 1.1.hello-world.ts
@@ -4,7 +4,6 @@ import type { Equal, Expect, NotAny } from '@type-challenges/utils';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>第一题:Hello World</h1><p> 你需要使得14行不会抛出异常</p>`;
-
 // 期望是一个 string 类型
 type HelloWorld = any;
 //你需要使得如下这行不会抛出异常
