520a38b90fe0c9d060f6c8a62e43ddda9ecfc928
paiDaXing-web <43596552+paiDaXing-web@users.noreply.github.com>
2023-01-11 12:19:25 +0800

    1 file added

---
+++ 1.13.push.ts
@@ -0,0 +1,12 @@
+/* _____________ 你的代码 _____________ */
+
+type Push<T, U> = any;
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>
+];
