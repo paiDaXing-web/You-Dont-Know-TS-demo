27f06349199473c064a1f60886bdf9c363dfe7e3
paiDaXing-web <43596552+paiDaXing-web@users.noreply.github.com>
2023-01-11 12:24:56 +0800

    1 file added

---
+++ 1.14.unshift.ts
@@ -0,0 +1,13 @@
+
+/* _____________ 你的代码 _____________ */
+
+type Unshift<T, U> = any
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Unshift<[], 1>, [1]>>,
+  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
+  Expect<Equal<Unshift<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>,
+]
