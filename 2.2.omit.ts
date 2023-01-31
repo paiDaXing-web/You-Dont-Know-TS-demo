20e0e3efe0bf613c5b3c03ffc9524ece97b76a99
paiDaXing-web <43596552+paiDaXing-web@users.noreply.github.com>
2023-01-31 19:32:34 +0800

    1 file added

---
+++ 2.2.omit.ts
@@ -0,0 +1,29 @@
+/* _____________ 你的代码 _____________ */
+
+type MyOmit<T, K> = any;
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>
+];
+
+// @ts-expect-error
+type error = MyOmit<Todo, 'description' | 'invalid'>;
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+  completed: boolean;
+}
+
+interface Expected2 {
+  title: string;
+}
