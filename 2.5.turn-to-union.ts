c36b68af110dac956f9bdb63ca63abb22e22e0cf
paiDaXing-web <43596552+paiDaXing-web@users.noreply.github.com>
2023-02-12 21:57:42 +0800

    2 files added

---
+++ 2.5.turn-to-union.ts
@@ -0,0 +1,11 @@
+/* _____________ Your Code Here _____________ */
+
+type TupleToUnion<T> = any;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
+  Expect<Equal<TupleToUnion<[123]>, 123>>
+];
